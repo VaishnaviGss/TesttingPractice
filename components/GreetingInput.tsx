87ab4547d2e9f7865937b8513e130b73804409e5
vaishnavi <vaishnavi@gsstech.in>
2025-11-11 17:24:25 +0530

    Intital commit

---
+++ components/GreetingInput.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const GreetingInput = () => {
+  return (
+    <View>
+      <Text>GreetingInput</Text>
+      <Text>GreetingInput dkkvjdfhj</Text>
+    </View>
+  );
+};
+
+export default GreetingInput;
+
+const styles = StyleSheet.create({});
